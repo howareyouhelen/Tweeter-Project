bf92ff6692f83cabd51c6572fbca64f26ec0900c
Helen <howareyouhelen@hotmail.com>
2017-07-13 23:17:14 +0000

    added a mongo database, connected tweetr app with mongo db and not the hardcoded data

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,7 +1,8 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
+// const simulateDelay = require("./util/simulate-delay");
+
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
@@ -9,19 +10,37 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      db.collection("tweets").insertOne(newTweet);
+      // db.tweets.push(newTweet);
+      callback(null, true);
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
-    }
+      // simulateDelay(() => {
+      db.collection("tweets").find().toArray(callback);
+      // (err, tweets) => {
+      //   if (err) {
+      //     return callback(err);
+      //   }
+      //   const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      //   tweets.sort(sortNewestFirst);
+      //   callback(null, tweets);
+      // })
 
+    // });
+    },
+
+
+    // getTweets((err, tweets) => {
+    //   if (err) {
+    //     throw err;
+    //   }
+    //   console.log("Logging each tweet:");
+    //   for (let tweet of tweets) {
+    //     console.log(tweet);
+    //   }
+    //   db.close();
+    // })
   };
 }
